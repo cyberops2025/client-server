8577ff6abdbb59d5243306d69cafacea8bf6e703
Daniel <218683191+cyberops2025@users.noreply.github.com>
2025-07-06 15:26:38 -0700

    Re-order includes.

--- tcp_client.c
+++ tcp_client.c
@@ -1,8 +1,8 @@
+#include <errno.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#include <netdb.h>
 #include <unistd.h>
 
 void                validate_inputs                 (int argc);
