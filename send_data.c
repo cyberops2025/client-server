a89a5d20ec9b3f17d40c558bcf79232f6d1d217f
Daniel <218683191+cyberops2025@users.noreply.github.com>
2025-07-08 17:56:18 -0700

    Modify code formatting.

--- send_data.c
+++ send_data.c
@@ -4,11 +4,15 @@
 void send_data(int socket_peer, fd_set* reads) {
 
     if (FD_ISSET(0, reads)) {
+
         char read[4096];
+
         if (!fgets(read, 4096, stdin)) exit(1);
         printf("Sending %s", read);
+        
         int bytes_sent = send(socket_peer, read, strlen(read), 0);
         printf("Sent %d bytes.\n", bytes_sent);
+
     }
 
 }
