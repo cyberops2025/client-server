b0d99c8663da070e06142602429c419e1d0d9ae1
Daniel <218683191+cyberops2025@users.noreply.github.com>
2025-07-08 17:50:32 -0700

    Modify comments. Fix include.

--- tcp_client.c
+++ tcp_client.c
@@ -3,18 +3,16 @@
 #include "establish_connection.h"
 #include "select_or_error.h"
 #include "receive_data.h"
+#include "send_data.h"
 
 int main(int argc, char* argv[]) {
     
     validate_inputs(argc);
 
-    // These also need to be validated.
-    // Focused on learning basics. Will validate later.
-    char* hostname  = argv[1];
-    char* port      = argv[2];
+    char* hostname  = argv[1]; // Need to validate
+    char* port      = argv[2]; // Need to validate
     
     int socket_peer = establish_connection(hostname, port);
-
     printf("Connected.\n");
 
     while (1) {
